16d07e01e6a20712aa9e8895560ac07fab729d3e
Vaishnavi Barge <vaishnavibarge0@gmail.com>
2026-08-29 14:32:11 +0000

    add package

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -12,7 +12,7 @@ with src_reviews as (
 )
 
 SELECT 
-  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id, --IDE diagnostic/stale language-server issue
   *
 FROM src_reviews
 WHERE review_text is not null
